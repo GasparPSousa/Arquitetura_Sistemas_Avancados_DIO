21842865d83353f481eb9619fe22cd40b0f3a706
GasparPSousa <gasparufrj@gmail.com>
2021-03-26 14:34:05 -0300

    Conceitos de Data Warehouse

--- Cap02_Arquitetura_de_Dados_NaoEstruturados_e_BusinessIntelligence/README.md
+++ Cap02_Arquitetura_de_Dados_NaoEstruturados_e_BusinessIntelligence/README.md
@@ -1,3 +1,4 @@
 # Cap02 - Arquitetura de Dados Não Estruturados e Business Intelligence
 
 ### Aula01 - Business Intelligence em Modelos de Dados
+### Aula02 - Data Warehouse
---
+++ Cap02_Arquitetura_de_Dados_NaoEstruturados_e_BusinessIntelligence/aula2_DataWarehouse.py
@@ -0,0 +1,36 @@
+"""
+=========================================================================================
+                                    Data Warehouse
+=========================================================================================
+
+
+
+=============================== OLTP x OLAP ============================================
+
+OLTP                                                        OLAP
+
+On-Line TRANSACTION Processing                        On-Line ANALYTICAL Processing
+Público Alvo Grande                                     Público Alvo Reduzido
+Foco no Nível Operacional.                              Foco no Nível Estratégico
+Visa a Execução Operacional.                            Visa a Análise Empresarial e Tomada de Decisão
+Dados Voláteis passíveis de modificação e execução.     Dados Históricos e não voláteis.
+Operações PONTUAIS com Select/Update/Insert             Relatórios CONSOLIDADOS
+em cláusulas muito reduzidas                            Cláusulas grandes
+
+
+====================================== Data WareHouse ===================================
+
+OLTP                                                OLAP DW
+
+SQL?                                             Estrutura já consolidada
+
+
+A estrutura de Relacionamento ocupa processamento dentro do sistema.
+No Data Warehouse com a consolidação eu não tenho mais...então fica mais rápido as consultas.
+
+
+
+
+
+
+"""
