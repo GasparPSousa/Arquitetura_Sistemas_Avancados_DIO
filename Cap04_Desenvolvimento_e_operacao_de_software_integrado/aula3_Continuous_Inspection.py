9eb22f0788c86d93d3401ad576d20195f0274416
GasparPSousa <gasparufrj@gmail.com>
2021-03-27 14:09:10 -0300

    Continuous Inspection

---
+++ Cap04_Desenvolvimento_e_operacao_de_software_integrado/aula3_Continuous_Inspection.py
@@ -0,0 +1,62 @@
+"""
+================================================================================
+                Parte 3: Code Quality Analys Continuous Inspection
+================================================================================
+
+========================= Principais Ferramentas =============================
+
+sonarqube
+code climate
+Codacy
+
+
+========================= Complexidade Ciclomática ===========================
+
+O quanto caminhos independentes seu código pode seguir a partir de determinadado pedaço da sua aplicação.
+
+Quanto mais ramificação você tiver, maior a complexidade ciclomática.
+
+
+
+========================= Código Duplicado ==============================
+
+Independente de modificar as variáveis, eu sei que é um código duplicado.
+
+
+===================== Vulnerabilidade/Code Smell ==========================
+
+Ex: Ter um password amarrado no seu código
+Ex: Método muito grande.
+Ex: Quando um comando está comentado e não era para está.
+
+
+
+====================== Padronização e Estilo ============================
+
+
+================== Débito Técnico =====================================
+
+Especifica qto tempo vc levará para corrigir os erros técnicos.
+
+
+
+
+================= Cobertuta de Teste =================================
+
+Permite que visualise que qual fluxo foi testado ou não.
+
+
+================ Métricas =====================================
+
+Fazem parte do ciclo de feedbacks, só que esses são feedbacks antes mesmo do software entrar em produção.
+Se tiver nota baixa não permite que avance para a etapa de implementaçao.
+
+
+====================== Validando Pull-Requests =========================
+
+Especifica alguns scheks status necessários para fazer o pull-requests.
+
+
+
+
+"""
