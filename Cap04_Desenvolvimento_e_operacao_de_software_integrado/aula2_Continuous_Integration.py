ab4ea84abb0339169111cf75de8b13af18adbc76
GasparPSousa <gasparufrj@gmail.com>
2021-03-27 13:48:03 -0300

    Continuous Integration

---
+++ Cap04_Desenvolvimento_e_operacao_de_software_integrado/aula2_Continuous_Integration.py
@@ -0,0 +1,36 @@
+"""
+=============================================================================
+                    Parte 2: CI/CD Continuous Integration
+=============================================================================
+
+
+======================== CI vc CD ===========================================
+
+
+
+My Application >>>>>> Continuous  >>>>>>>> Continuous Delivery  >>>>>>>> Approve Deploy     >>>>> Application Deploy
+New Version 1.1       Integration >>>>>>>> Continuous Deployment>>>>>>>> Automatic Deploy   >>>>> Application Deploy
+
+                      Build                                                                         Integration Tests
+                      Unit Tests                                                                    Load Tests
+                      QA
+                      Empacotamento
+
+
+A integração contínua tem como limite a geração do artefato, ela não faz a implementação em produção.
+
+
+============================ Principais Ferramentas ===============================================
+
+circleci
+AppVeyor
+Azure Pipelines
+GitLab CI
+Travis CI
+JenKins
+
+========================= Pipelanes =================================================
+
+
+
+"""
