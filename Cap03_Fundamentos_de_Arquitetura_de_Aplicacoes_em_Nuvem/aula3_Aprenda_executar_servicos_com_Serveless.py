6618c50b92f628d3a1d48ec758cd5d99b7e86f96
GasparPSousa <gasparufrj@gmail.com>
2021-03-27 11:50:52 -0300

    Aprenda a executar servicos com Serveless

---
+++ Cap03_Fundamentos_de_Arquitetura_de_Aplicacoes_em_Nuvem/aula3_Aprenda_executar_servicos_com_Serveless.py
@@ -0,0 +1,41 @@
+"""
+===============================================================================
+                                Serveless
+===============================================================================
+
+Vantagens:
+
+- Sem Servidor
+
+- Sem serviço rodando
+
+- Sem down time
+
+
+- API Gateway (Se eu quiser um API REST)
+- Amazon Kinesis (Serviço de Mensageria)
+- AWS  Lambda
+- Amazon S3 (Arquivos Estáticos)
+
+
+Desvantagens:
+
+Custo acaba sendo maior pq vou ter que pagar por essa abstração.
+Número maior de Lambdas.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+"""
