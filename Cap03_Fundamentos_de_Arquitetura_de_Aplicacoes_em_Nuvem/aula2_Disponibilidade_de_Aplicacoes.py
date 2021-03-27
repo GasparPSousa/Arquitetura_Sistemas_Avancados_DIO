69fa240c182813f86882fddaf67102fa4cbb8e0e
GasparPSousa <gasparufrj@gmail.com>
2021-03-27 11:38:06 -0300

    Disponibilidade

---
+++ Cap03_Fundamentos_de_Arquitetura_de_Aplicacoes_em_Nuvem/aula2_Disponibilidade_de_Aplicacoes.py
@@ -0,0 +1,36 @@
+"""
+========================================================================================
+                                   Disponibilidade
+========================================================================================
+
+IaaS - hardware e internet
+
+
+PaaS - auto scale in the go
+
+
+BaaS - não há backend service
+
+
+
+=============================== Disponibilidade ========================================
+
+- Kubernets (K8S)
+
+- Multiplos Nodos
+
+- Load Balancer
+
+Número mínimos de instância de 3
+Número mínimos de nodos de 3
+
+
+
+
+
+
+
+
+
+
+"""
